f6122a300e8dd1263f76dee2fb55be22c2313465
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2021-10-12 21:34:21 -0300

    ensure FacebookApi returns undefined when HttpGetClient throws

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FB_CLIENT_ID ?? '575893240358378',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? 'b74c96884decdd94af69ae69cd607ee2'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,35 @@
-describe('', () => {
-  it('', () => {
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
 
+describe('FacebookApi integration Tests', () => {
+  it('should return a Facebook User if token is valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'EAAILxZAgFJeoBAJBZBAr1ktBJqurlPcYdoc6BpPZCzqjZAHx3KRunYOtnWwAkXqblxpR4IBsZBT1UAEFhmR7nRc58KKlfNiF9XHWUjY67rEtip7G15AiHVbb14A9nRkpe9VZA0sryHjSteHlAFUofhuTEZAE2KYde50SsIkYJHZBwnPeZAeqZBDxLZAMzA5VZBjZBdiH02ZBppv8iNCLVl3Q8BnZAYB' })
+
+    expect(fbUser).toEqual({
+      facebookId: '105375675261539',
+      email: 'mango_ndcrpvh_teste@tfbnw.net',
+      name: 'Mango teste'
+    })
+  })
+
+  it('should return undefined if token is invvalid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'invalid' })
+
+    expect(fbUser).toBeUndefined()
   })
 })
