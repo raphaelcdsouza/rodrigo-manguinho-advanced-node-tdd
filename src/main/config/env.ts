ddeca14353053459b140e2e893123d23ed1350b8
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2021-11-30 09:43:07 -0300

    add aws s3 integration test

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,5 +4,10 @@ export const env = {
     clientSecret: process.env.FB_CLIENT_SECRET ?? 'b74c96884decdd94af69ae69cd607ee2'
   },
   port: process.env.PORT ?? 8080,
-  jwtSecret: process.env.JWT_SECRET ?? '1j32j123wqjwle'
+  jwtSecret: process.env.JWT_SECRET ?? '1j32j123wqjwle',
+  s3: {
+    accessKey: process.env.AWS_S3_ACCESS_KEY ?? 'AKIAX6ZRHVCYLGG5S4OZ',
+    secret: process.env.AWS_S3_SECRET ?? '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',
+    bucket: process.env.AWS_S3_BUCKET ?? 'manguinho-teste'
+  }
 }
