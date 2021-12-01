8f213ef807915d7397bf295bb8f845b4aa51972a
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2021-12-01 11:03:58 -0300

    change env variables to .env file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,13 +1,13 @@
 export const env = {
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '575893240358378',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? 'b74c96884decdd94af69ae69cd607ee2'
+    clientId: process.env.FB_CLIENT_ID ?? '',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? ''
   },
   port: process.env.PORT ?? 8080,
-  jwtSecret: process.env.JWT_SECRET ?? '1j32j123wqjwle',
+  jwtSecret: process.env.JWT_SECRET ?? '',
   s3: {
-    accessKey: process.env.AWS_S3_ACCESS_KEY ?? 'AKIAX6ZRHVCYLGG5S4OZ',
-    secret: process.env.AWS_S3_SECRET ?? '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',
-    bucket: process.env.AWS_S3_BUCKET ?? 'manguinho-teste'
+    accessKey: process.env.AWS_S3_ACCESS_KEY ?? '',
+    secret: process.env.AWS_S3_SECRET ?? '',
+    bucket: process.env.AWS_S3_BUCKET ?? ''
   }
 }
